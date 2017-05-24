24d4cbde225dd99879f7f2d75b1461628d430a06
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2017-05-24 11:14:33 +0200

    feat: add --exact option (in index.js)

--- index.js
+++ index.js
@@ -25,61 +25,70 @@ let execP = command => {
 function checkDirectory(d, options) {
   let spinner = ora(`${d} - Checking`).start();
   let command = `cd ${d} && git status`;
-  return execP(command).then(({ stdout }) => {
-    let isClean = new RegExp("nothing to commit");
-    let toCommit = new RegExp("Changes not staged for commit");
-    let commitAhead = new RegExp("Your branch is ahead");
-    let untrackedFiles = new RegExp("untracked files present");
-    if (toCommit.test(stdout) || untrackedFiles.test(stdout)) {
-      spinner.fail(`${d} - Not clean.`);
-    } else {
-      if (commitAhead.test(stdout)) {
-        if (options.push) {
-          spinner.info(`${d} - Forcing push`);
-          let pspinner = ora(`${d} - Pushing`).start();
-          let cmd1 = `cd ${d} && git push`;
-          return execP(cmd1).then(
-            () => {
-              pspinner.succeed(`${d} - Correctly pushed`);
-            },
-            ({ stderr }) => {
-              pspinner.fail(`${d} - Got error ${stderr}`);
-            }
-          );
-        } else {
-          spinner.warn(`${d} - Some commits should be pushed into remote`);
-        }
+  return execP(command).then(
+    ({ stdout }) => {
+      let isClean = new RegExp("nothing to commit");
+      let toCommit = new RegExp("Changes not staged for commit");
+      let commitAhead = new RegExp("Your branch is ahead");
+      let untrackedFiles = new RegExp("untracked files present");
+      if (toCommit.test(stdout) || untrackedFiles.test(stdout)) {
+        spinner.fail(`${d} - Not clean.`);
       } else {
-        if (isClean.test(stdout)) {
-          spinner.succeed(`${d}`);
+        if (commitAhead.test(stdout)) {
+          if (options.push) {
+            spinner.info(`${d} - Forcing push`);
+            let pspinner = ora(`${d} - Pushing`).start();
+            let cmd1 = `cd ${d} && git push`;
+            return execP(cmd1).then(
+              () => {
+                pspinner.succeed(`${d} - Correctly pushed`);
+              },
+              ({ stderr }) => {
+                pspinner.fail(`${d} - Got error ${stderr}`);
+              }
+            );
+          } else {
+            spinner.warn(`${d} - Some commits should be pushed into remote`);
+          }
         } else {
-          spinner.info(`${d} - Unknown result - ${stdout}`);
+          if (isClean.test(stdout)) {
+            spinner.succeed(`${d}`);
+          } else {
+            spinner.info(`${d} - Unknown result - ${stdout}`);
+          }
         }
       }
+    },
+    ({ stderr }) => {
+      spinner.info(`${d} - Skipped as probably not a git repo`);
     }
-  });
+  );
 }
 
 function main(target, options) {
   if (!path.isAbsolute(target)) {
     target = path.resolve(process.cwd(), target);
   }
-  let files = $s.ls("-dl", `${target}/*`);
-  let directories = _.filter(files, f => f.isDirectory());
-  directories = _.filter(directories, f => {
-    let ref = $m().subtract(options.from, "day");
-    let ft = $m(f.mtime);
-    if ($m(f.mtime).isAfter(ref)) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-  Promise.all(
-    _.map(directories, d => {
-      checkDirectory(d.name, options);
-    })
-  );
+  if (!options.exact) {
+    let files = $s.ls("-dl", `${target}/*`);
+    let directories = _.filter(files, f => f.isDirectory());
+    directories = _.filter(directories, f => {
+      let ref = $m().subtract(options.from, "day");
+      let ft = $m(f.mtime);
+      if ($m(f.mtime).isAfter(ref)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return Promise.all(
+      _.map(directories, d => {
+        checkDirectory(d.name, options);
+      })
+    );
+  } else {
+    return checkDirectory(target, options);
+  }
 }
 
 prog
@@ -88,8 +97,9 @@ prog
   .argument("<target...>", "target directory")
   .option("--from <days>", "Look <days> behind", prog.INT, 10)
   .option("--push", "Force push of branches that are ahead")
+  .option("--exact", "Targets are already git level repos")
   .action(function({ target }, options) {
-    Promise.all(
+    return Promise.all(
       _.map(target, t => {
         main(t, options);
       })
